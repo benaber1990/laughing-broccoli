02fc449ec0340361fc71f26e644cb86f5d972c51
benaber1990 <benaber1990@gmail.com>
2024-01-09 11:47:47 +0000

    new

--- firebase.config.js
+++ firebase.config.js
@@ -4,12 +4,12 @@ import "firebase/compat/auth"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-  apiKey: process.env.GATSBY_FIREBASE_API_KEY,
-  authDomain: process.env.GATSBY_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.GATSBY_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.GATSBY_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.GATSBY_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.GATSBY_FIREBASE_APP_ID,
+  apiKey: "AIzaSyChtonwBnG-Jzs-gMJRbTChiv-mwt13rNY",
+  authDomain: "unis-1.firebaseapp.com",
+  projectId: "unis-1",
+  storageBucket: "unis-1.appspot.com",
+  messagingSenderId: "`500039576121`",
+  appId: "1:500039576121:web:af595bd3bc72422d4fbbe8",
 }
 
 /**Initialising App */
